3dc83afa2eaf956d844580daeb31f4898d1d158a
azagorowski <37112877+azagorowski@users.noreply.github.com>
2018-03-20 18:31:02 +0100

    Update drawing-program.js

--- drawing-program.js
+++ drawing-program.js
@@ -3,7 +3,7 @@ const draw = function(evt) {
 	if (active == false) return;
 	const x = evt.clientX;
 	const y = evt.clientY;
-	const div = document.createElement("div");
+	const div = document.createElement("colorRed");
 	div.style.top = y + "px";
 	div.style.left = x + "px";
 	document.body.appendChild(div);
